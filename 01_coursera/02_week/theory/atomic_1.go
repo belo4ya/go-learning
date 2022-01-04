726efb8ab1d2a4aae46cd3fd297fc70d2f999080
belo4ya <41exey.kov41ev@gmail.com>
2022-01-04 23:59:51 +0300

    theory notes 1&2

--- 01_coursera/02_week/theory/atomic_1.go
+++ 01_coursera/02_week/theory/atomic_1.go
@@ -5,17 +5,17 @@ import (
 	"time"
 )
 
-var totalOperations int32 = 0
-
-func inc() {
-	totalOperations++
-}
-
 func main() {
+	var totalOperations int32 = 0
+
+	inc := func() {
+		totalOperations++
+	}
+
 	for i := 0; i < 1000; i++ {
 		go inc()
 	}
 	time.Sleep(2 * time.Millisecond)
-	// ождается 1000, но по факту будет меньше
+	// ожидается 1000, но по факту будет меньше
 	fmt.Println("total operation = ", totalOperations)
 }
--- 01_coursera/02_week/theory/atomic_2.go
+++ 01_coursera/02_week/theory/atomic_2.go
@@ -6,13 +6,13 @@ import (
 	"time"
 )
 
-var totalOperations int32
-
-func inc() {
-	atomic.AddInt32(&totalOperations, 1) // автомарно
-}
-
 func main() {
+	var totalOperations int32 = 0
+
+	inc := func() {
+		atomic.AddInt32(&totalOperations, 1) // атомарно
+	}
+
 	for i := 0; i < 1000; i++ {
 		go inc()
 	}
